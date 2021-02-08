2111d3f7ce693fcfa4c83ec75dfb164800147456
Ella Berg <ellamadelineberg@gmail.com>
2021-02-08 20:35:38 +0200

    Add home routing

--- darksoulswiki/src/main/java/fi/mdeline/darksoulswiki/DarksoulswikiApplication.java
+++ darksoulswiki/src/main/java/fi/mdeline/darksoulswiki/DarksoulswikiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DarksoulswikiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DarksoulswikiApplication.class, args);
 	}
-
 }
