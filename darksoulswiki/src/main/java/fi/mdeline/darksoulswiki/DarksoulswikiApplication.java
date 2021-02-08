5c3cd2e8442bc1012e057b4b1763337d96bb4589
Ella Berg <ellamadelineberg@gmail.com>
2021-02-08 20:10:19 +0200

    Initialize a new Spring Boot app

---
+++ darksoulswiki/src/main/java/fi/mdeline/darksoulswiki/DarksoulswikiApplication.java
@@ -0,0 +1,13 @@
+package fi.mdeline.darksoulswiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarksoulswikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarksoulswikiApplication.class, args);
+	}
+
+}
---
+++ darksoulswiki/src/test/java/fi/mdeline/darksoulswiki/DarksoulswikiApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.mdeline.darksoulswiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarksoulswikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
