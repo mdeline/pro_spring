f7b629d9e7e5cf6c82fc83a4451d74934987332f
Ella Berg <ellamadelineberg@gmail.com>
2021-02-11 22:02:59 +0200

    Add a new end point, refactor the old one

--- darksoulswiki/src/main/java/fi/mdeline/darksoulswiki/HomeController.java
+++ darksoulswiki/src/main/java/fi/mdeline/darksoulswiki/HomeController.java
@@ -2,11 +2,18 @@ package fi.mdeline.darksoulswiki;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
     @GetMapping("*")
+    @ResponseBody
     public String home() {
-        return "home";
+        return "<h1>Dark Souls Wiki</h1>";
+    }
+
+    @GetMapping("/development")
+    public String development() {
+        return "dev_concepts";
     }
 }
