73d510dde810d981b5cf4898814ee626daee859d
Ella Berg <ellamadelineberg@gmail.com>
2021-02-15 12:09:21 +0200

    Add a form for blog texts

--- darksoulswiki/src/main/java/fi/mdeline/darksoulswiki/HomeController.java
+++ darksoulswiki/src/main/java/fi/mdeline/darksoulswiki/HomeController.java
@@ -1,19 +1,30 @@
 package fi.mdeline.darksoulswiki;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
-    @GetMapping("*")
-    @ResponseBody
-    public String home() {
-        return "<h1>Dark Souls Wiki</h1>";
+    private String content = "plz help me";
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("content", content);
+        return "home";
+    }
+
+    @PostMapping("/")
+    public String post(@RequestParam String content) {
+        this.content = content;
+        return "redirect:/";
     }
 
     @GetMapping("/development")
     public String development() {
         return "dev_concepts";
     }
+
 }
